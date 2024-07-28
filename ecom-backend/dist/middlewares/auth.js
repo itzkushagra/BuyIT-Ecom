6255c65d53ae8281b188df2d76eed0b33ec8c92d
itzkushagra <info.kushagrapandey@gmail.com>
2024-07-28 22:58:46 +0530

    completed custom authentication middleware

---
+++ ecom-backend/dist/middlewares/auth.js
@@ -0,0 +1,15 @@
+//custom handler to allow only admin to access admin rights
+import { User } from "../models/user.js";
+import ErrorHandler from "../utils/utilityclass.js";
+import { TryCatch } from "./error.js";
+export const adminOnly = TryCatch(async (req, res, next) => {
+    const { id } = req.query;
+    if (!id)
+        return next(new ErrorHandler("Please Login", 401));
+    const user = await User.findById(id);
+    if (!user)
+        return next(new ErrorHandler("User doesn't exist", 401));
+    if (user.role !== "admin")
+        return next(new ErrorHandler("You dont have access to this feature", 403));
+    next();
+});
--- ecom-backend/src/routes/user.ts
+++ ecom-backend/src/routes/user.ts
@@ -1,10 +1,15 @@
 import express from "express";
-import { newUser } from "../controllers/user.js";
+import { deleteUser, getAllUsers, newUser } from "../controllers/user.js";
+import { adminOnly } from "../middlewares/auth.js";
+
 
 const app = express.Router();
 
 //route -/api/v1/user/new
-app.post("/new",newUser);
+app.post("/new", newUser);
+
+app.route("/:id").get(adminOnly,getAllUsers).delete(adminOnly, deleteUser);
+ 
 
 
 export default app;
