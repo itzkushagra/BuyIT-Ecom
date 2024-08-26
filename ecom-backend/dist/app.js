a85f30213605acff02b1dfa981e4fbc2835c1c8c
itzkushagra <info.kushagrapandey@gmail.com>
2024-08-27 02:23:01 +0530

    DONE

--- ecom-backend/dist/app.js
+++ ecom-backend/dist/app.js
@@ -5,6 +5,7 @@ import NodeCache from 'node-cache';
 //importing routes
 import userRoutes from "./routes/user.js";
 import productRoutes from "./routes/products.js";
+import orderRoute from "./routes/orders.js";
 const port = 3000;
 connectDB();
 export const myCache = new NodeCache();
@@ -16,6 +17,7 @@ app.get("/", (req, res) => {
 //use of routes
 app.use("/api/v1/user", userRoutes);
 app.use("/api/v1/product", productRoutes);
+app.use("/api/v1/order", orderRoute);
 app.use("/uploads", express.static("uploads"));
 //use to get error in the beginning, when next is called in user.ts\controller the next function will execute this
 app.use(errorMiddleware);
